10e59b0547714e8b9c8646b2b85e690cc54e89cf
Moqleh <moh.oglah@gmail.com>
2026-09-15 01:40:49 +0300

    Cache dedicated bench press images

--- mo-fitness/sw.js
+++ mo-fitness/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mo-fitness-v6';
-const CORE=['./','./index.html','./exercise-guide.js','./privacy.html','./disclaimer.html','./manifest.json','./icon.svg'];
+const CACHE='mo-fitness-v7';
+const CORE=['./','./index.html','./exercise-guide.js','./privacy.html','./disclaimer.html','./manifest.json','./icon.svg','./assets/exercises/bench-press/start.jpg','./assets/exercises/bench-press/descent.jpg','./assets/exercises/bench-press/finish.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
